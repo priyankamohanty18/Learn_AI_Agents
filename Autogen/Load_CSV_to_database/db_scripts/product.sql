ba39879a3ca5afa0d86b472de47d97ab26b29ca4
priyankamohanty18 <50870196+priyankamohanty18@users.noreply.github.com>
2025-04-20 13:09:18 +0100

    Add files via upload

---
+++ Autogen/Load_CSV_to_database/db_scripts/product.sql
@@ -0,0 +1,11 @@
+CREATE TABLE product
+(
+    Product_ID integer,
+    Product_Name    text,
+    Category    text,
+    Price	float,
+    Stock_Quantity integer,	
+    Supplier	text,	
+    Launch_Date	date,
+    Rating float
+);
